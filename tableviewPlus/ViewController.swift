1feaf3f7cf71ddb219899b8ef5c17c7e7791a4b7
eleves <eleves@mac404inf-prof.local>
2017-07-17 16:15:18 -0400

    v1

--- tableviewPlus/ViewController.swift
+++ tableviewPlus/ViewController.swift
@@ -8,18 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+    @IBOutlet weak var tableview: UITableView!
+    var aDict: [String: Bool] = ["String 1" : false, "String 2" : true, "String 3" : false]
+    var keys: [String] = []
+    var values: [Bool] = []
+    
+    func parseDict() {
+        keys = []
+        values = []
+        for(k, v) in aDict {
+            keys.append(k)
+            values.append(v)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        parseDict()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    //---------------------
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tableView.backgroundColor = UIColor.clear
+        return aDict.count
+    }
+    //---------------------
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = UITableViewCell(style:UITableViewCellStyle.default, reuseIdentifier:"proto")
+        cell.textLabel!.text = keys[indexPath.row]
+        cell.textLabel?.textColor = UIColor.black
+        cell.backgroundColor = UIColor.clear
+        
+        return cell
+    }
+    //---------------------
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if values[indexPath.row] {
+            cell.backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    //---------------------
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedCell:UITableViewCell = tableView.cellForRow(at: indexPath as IndexPath)!
+        selectedCell.contentView.backgroundColor = UIColor.darkGray
+        if !values[indexPath.row] {
+            values[indexPath.row] = true
+        } else {
+            values[indexPath.row] = false
+        }
+        tableView.reloadData()
+    }
+    //---------------------
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            tableView.deleteRows(at: [indexPath as IndexPath], with: UITableViewRowAnimation.automatic)
+        }
+    }
+    //---------------------
 }
 
